bc84e15539dd8c6c74af750d485606f73f0f04d1
somesh9870 <someshrawat106.com>
2023-04-08 23:26:00 +0530

    added assets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App cursor-wait h-screen ">
       <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <div className="h-screen flex justify-center items-center cursor-custom">
-      <p>Hover over me to see the custom cursor!</p>
-    </div>
-    </h1>
+        Hello world!
+        <div className="h-screen flex justify-center items-center cursor-custom">
+          <p>Hover over me to see the custom cursor!</p>
+        </div>
+      </h1>
     </div>
   );
 }
