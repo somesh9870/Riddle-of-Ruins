36bce8eb612331599f548f25b2d3e291096e5334
Ragini Kaushal <ragini93.11@gmail.com>
2023-04-08 11:48:54 +0530

    add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App cursor-wait h-screen ">
       <h1 className="text-3xl font-bold underline">
       Hello world!
+      <div className="h-screen flex justify-center items-center cursor-custom">
+      <p>Hover over me to see the custom cursor!</p>
+    </div>
     </h1>
     </div>
   );
