505d16d08b7dee92a1a0f87b38c7b3c509390daf
udaykashyap <udaykashyap.hjp@gmail.com>
2023-04-11 14:08:29 +0530

    sound fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,12 @@ import music from "./Assets/music.mp3";
 
 function App() {
   const audio = new Audio(music);
+  audio.loop = true;
   audio.play();
+  // <audio controls loop>
+  //   <source src={music} type="audio/mpeg" />
+  //   Your browser does not support the audio element.
+  // </audio>;
   return (
     <div className="App  h-screen ">
       <AllRoutes />
